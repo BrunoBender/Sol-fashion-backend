97fb2a1dda01ab735b8064c18e818d710f5fa8cc
Bruno Paulo Bender <brunopaulobender@gmail.com>
2021-06-09 14:24:49 -0300

    Upload Project

---
+++ src/main/java/br/com/senior/solFashion/SolFashionApplication.java
@@ -0,0 +1,13 @@
+package br.com.senior.solFashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolFashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolFashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/senior/solFashion/SolFashionApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senior.solFashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolFashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
